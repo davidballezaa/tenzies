dcd639c7093c114f7b8db65c309c1de77b12f99a
davidballezaa <davidballezaa@gmail.com>
2022-02-10 16:25:58 -0600

    add stats section and current role count

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ import Confetti from 'react-confetti'
 function App() {
   const [dice, setDice] = useState(AllNewDice())
   const [tenzies, setTenzies] = useState(false)
+  const [rollCount, setRollCount] = useState(0)
+  const [seeStats, setSeeStats] = useState(false)
+  const [stats, setStats] = useState([])
 
   useEffect(() => {
     let value = dice[0].value
     if (
-      dice.every(die => die.value == value) &&
+      dice.every(die => die.value === value) &&
       dice.every(die => die.isHeld)
     ) {
       setTenzies(true)
     }
   }, [dice])
+
+  useEffect(() => {
+    if (tenzies) {
+      setStats(prevStats => [
+        {
+          id: nanoid(),
+          rollCount: rollCount,
+          date: new Date().toLocaleString('en-US')
+        },
+        ...prevStats
+      ])
+    }
+  }, [tenzies])
 
   function AllNewDice() {
     let newDice = []
@@ -34,20 +50,31 @@ function App() {
   }
 
   function changeHeld(id) {
-    setDice(prevDice =>
-      prevDice.map(die =>
-        die.id == id ? { ...die, isHeld: !die.isHeld } : die
+    if (!tenzies) {
+      setDice(prevDice =>
+        prevDice.map(die =>
+          die.id === id ? { ...die, isHeld: !die.isHeld } : die
+        )
       )
-    )
+    }
   }
 
   function rollDice() {
     if (!tenzies) {
       setDice(prevDice => prevDice.map(die => (die.isHeld ? die : createDie())))
+      setRollCount(prevCount => prevCount + 1)
     } else {
       setTenzies(false)
+      setRollCount(0)
       setDice(AllNewDice())
     }
+  }
+
+  function handlePlayAgain() {
+    setSeeStats(false)
+    setTenzies(false)
+    setRollCount(0)
+    setDice(AllNewDice())
   }
 
   const diceElements = dice.map(die => (
@@ -59,24 +86,58 @@ function App() {
     />
   ))
 
-  return (
-    <main>
-      {tenzies && <Confetti />}
-      <div className="dice">
-        <div className="dice--header">
-          <h1>Tenzies</h1>
-          <h2>
-            Roll until all dice are the same. Click each die to freeze it at its
-            current value between rolls.
-          </h2>
+  if (!seeStats) {
+    return (
+      <main>
+        {tenzies && <Confetti />}
+        <div className="dice">
+          <div className="dice--header">
+            <h1>Tenzies</h1>
+            <h2>
+              Roll until all dice are the same. Click each die to freeze it at
+              its current value between rolls.
+            </h2>
+            {!tenzies && <div>Current Rolles: {rollCount}</div>}
+          </div>
+          <div className="dice--container">{diceElements}</div>
+          <div className="buttons--container">
+            <button className="dice--button button" onClick={rollDice}>
+              {tenzies ? 'Play Again' : 'Roll'}
+            </button>
+            <button
+              className="button stats--button"
+              onClick={() => setSeeStats(true)}
+            >
+              Stats
+            </button>
+          </div>
         </div>
-        <div className="dice--container">{diceElements}</div>
-        <button className="dice--button" onClick={rollDice}>
-          {tenzies ? 'Play Again' : 'Roll'}
-        </button>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  } else {
+    return (
+      <main>
+        <div className="stats--container" style={{ textAlign: 'center' }}>
+          <h2>My Stats</h2>
+          {stats.length > 0 ? (
+            stats.map(item => (
+              <div key={item.id}>
+                <p className="stats--date">
+                  {item.date}{' '}
+                  <span className="stats--count">{item.rollCount} rolls</span>
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>It seems like you don't have any stats yet...</p>
+          )}
+          <button className="button back--button" onClick={handlePlayAgain}>
+            Play Again
+          </button>
+        </div>
+      </main>
+    )
+  }
 }
 
 export default App
